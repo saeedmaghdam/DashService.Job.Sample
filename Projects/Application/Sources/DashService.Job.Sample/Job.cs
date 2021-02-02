4fdaa2ba3ad2472f5d0192fffa3bf0fc04883a97
saeedmaghdam <me.register@outlook.com>
2021-02-03 01:20:21 +0330

    Implemented sample job

---
+++ Projects/Application/Sources/DashService.Job.Sample/Job.cs
@@ -0,0 +1,32 @@
+﻿using DashService.Logger;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DashService.Job.Sample
+{
+    public class Job : JobBase
+    {
+        private readonly ILogger _logger;
+
+        public virtual string Name => "Sample Job";
+        public virtual string Description => "Test Job";
+        public virtual string Version => "1.0.0";
+
+        public Job(ILogger logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.Information("Hello, I'm the sample job!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.Information("Thank you, bye.");
+            return Task.CompletedTask;
+        }
+    }
+}
