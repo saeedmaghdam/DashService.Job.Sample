2413a627ce08f508746f31f7bd1d66cc8df31c7d
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-02-03 16:46:25 +0330

    Improved sample job

--- Projects/Application/Sources/DashService.Job.Sample/Job.cs
+++ Projects/Application/Sources/DashService.Job.Sample/Job.cs
@@ -21,12 +21,25 @@ namespace DashService.Job.Sample
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.Information("Hello, I'm the sample job!");
+
+            do
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    break;
+
+                _logger.Information("I'm sample job, I'm doing a process ...");
+
+                Task.Delay(3000, cancellationToken).Wait(cancellationToken);
+            }
+            while (true);
+
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _logger.Information("Thank you, bye.");
+            _logger.Information("I'm sample job! I've finished my task, thank you.");
+
             return Task.CompletedTask;
         }
     }
